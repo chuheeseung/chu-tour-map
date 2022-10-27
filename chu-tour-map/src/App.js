97f375408a8490f32b2741f324de0778331927a9
chuheeseung <youverygood@naver.com>
2022-10-28 00:07:46 +0900

    22.10.27 - 리액트 컴포넌트 생성 - naver map api 연동 진행중

--- chu-tour-map/src/App.js
+++ chu-tour-map/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="home" element={<HomePage />} />
+				<Route path="/" element={<HomePage />} />
 			</Routes>
 		</BrowserRouter>
 	);
--- chu-tour-map/src/components/HomePage/MenuBar.jsx
+++ chu-tour-map/src/components/HomePage/MenuBar.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { NaverMap, RenderAfterNavermapsLoaded } from 'react-naver-maps';
 
 const MenuBar = () => {
-	return <div>MenuBar</div>;
+	return (
+		<RenderAfterNavermapsLoaded
+			ncpClientId={process.env.REACT_APP_API_KEY}
+			error={<p>Maps Load Error</p>}
+			loading={<p>Maps Loading...</p>}
+		>
+			<NaverMap
+				mapDivId="map"
+				defaultCenter={{ lat: 37.49988, lng: 127.03856 }}
+				defaultZoom={16}
+				zoomControl={true}
+			/>
+		</RenderAfterNavermapsLoaded>
+	);
 };
 
 export default MenuBar;
